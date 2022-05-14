9d6706456d3d6d192da414f2441eb6d5bfdb2cc8
Antoine Pays <jototo750@gmail.com>
2022-05-14 11:55:32 +0200

    manager

---
+++ BusinessLayer/Manager.cs
@@ -0,0 +1,20 @@
+﻿using Jalon1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Manager
+    {
+        private readonly Context context;
+        private static Manager _manager = null;
+
+        public Manager()
+        {
+            context = new Context();
+        }
+    }
+}
